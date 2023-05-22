46f7fe8d70cd4b5e0e03fa460dc89430201fe015
jojo <116190164+jojodkks@users.noreply.github.com>
2023-05-22 07:58:44 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,17 @@
+
+function ativarNome(){
+    let nome = 'jojo';
+    document.write(nome)
+    console.log(nome)
+
+}
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10; 
+    let c = a+b;
+    //console.log(c)
+    //alert(c)
+    document.getElementById('aparecer').innerHTML = c
+
+}
